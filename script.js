506e5fcc06d4b17221f3cc6fe64efd7947c9704e
shivPosted <shivpratap621@gmail.com>
2023-10-02 23:55:25 +0530

    demo01

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 ///////////////////////////////////////
 // Modal window
+const click = document.querySelector('.click-me');
+
+const tags = ['flowers', 'animals', 'beach', 'amoled', 'anime'];
+click.addEventListener('onclick', () => {
+  const rn = Math.floor(Math.random() * 5);
+  console.log(rn);
+});
